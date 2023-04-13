ad3e4e3257781af18cd109654950ff89b6b01be7
aaronMan12 <aaronhernandez597@gmail.com>
2023-04-13 15:22:07 -0600

    microservicio de busqueda por carrera

---
+++ PorCarrera/src/test/java/mx/uv/apidispersa/ApiDispersaApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uv.apidispersa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDispersaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
